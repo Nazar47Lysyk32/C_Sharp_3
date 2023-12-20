2a618c479cdf99e84633c8a3f71904b3c7fe33ff
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2023-12-20 18:26:42 +0200

    Add files via upload

---
+++ Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Інтерфейс для виводу інформації
+public interface IOutput
+{
+    void Show();           
+    void Show(string info); 
+}
+
+// Клас, що реалізує інтерфейс IOutput
+public class ConsoleOutput : IOutput
+{
+    public void Show()
+    {
+        Console.WriteLine("Output of information without additional message.");
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine($"Information output: {info}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IOutput output = new ConsoleOutput();
+
+        // Виклики методів для виводу інформації
+        output.Show();
+        output.Show("This is an additional message.");
+    }
+}
